3dce473a832bda77f3843aa28351deac8607b604
Arpit Abhyankar <arpitabhyankar99823@gmail.com>
2022-04-22 17:16:36 +0530

    testing

--- DjangoEcom/settings.py
+++ DjangoEcom/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1wgpp09l58c^$_az1+6@8s5$thz$j$2-nv328$-fk%wks+#at2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['appify1702.herokuapp.com','127.0.0.1']
 
 # Application definition
 
